88a8c77609cf20f908cbd363086fb40bb6b69fab
vargattj <vargattjh@gmail.com>
2020-07-17 21:15:38 -0300

    first

---
+++ src/api/index.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async () => {
+  try {
+    const { data } = await axios.get(url);
+
+    const { confirmed, recovered, deaths, lastUpdate } = data;
+
+    return {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+  } catch (error) {}
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (e) {}
+};
+
+export const countries = async () => {
+  try {
+    const response = await axios.get(`${url}/countries`);
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
